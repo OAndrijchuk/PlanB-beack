35ef2f75138cd8c37f40db5725b4f32dab5d8a4a
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2024-06-03 01:22:37 +0300

    try4

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
+// import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  // app.use(cookieParser());
   app.enableCors({
     origin: '*',
-    credentials: true,
+    // credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
